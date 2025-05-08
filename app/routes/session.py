771c7b7e8bc78ce4b0677c722bcf395cefc1f22d
unknown <nabil.mortis@gmail.com>
2025-05-08 23:20:26 +0200

    Add await to delete session

--- app/routes/session.py
+++ app/routes/session.py
@@ -6,15 +6,15 @@ r = redis.Redis()
 
 @router.post("/reset")
 async def reset_session(user_id: str = Form(...)):
-    r.delete(f"session:{user_id}")
-    r.delete(f"chat:{user_id}")
-    r.delete(f"audio:{user_id}")
-    r.delete(f"image:{user_id}")
+    await r.delete(f"session:{user_id}")
+    await r.delete(f"chat:{user_id}")
+    await r.delete(f"audio:{user_id}")
+    await r.delete(f"image:{user_id}")
     return {"message": "Session reset successful."}
 
 @router.post("/resetscenario")
 async def reset_session(user_id: str = Form(...)):
-    r.delete(f"chat:{user_id}")
-    r.delete(f"audio:{user_id}")
-    r.delete(f"image:{user_id}")
+    await r.delete(f"chat:{user_id}")
+    await r.delete(f"audio:{user_id}")
+    await r.delete(f"image:{user_id}")
     return {"message": "Session scenario reset successfully."}
